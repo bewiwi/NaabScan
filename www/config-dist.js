920e2bef930ef66527fc5fe72383a5aa2140000c
Bewiwi <bewiwi@bibabox.fr>
2013-08-24 17:07:22 +0200

    Move api to a new repo

--- www/config-dist.js
+++
@@ -1,9 +0,0 @@
-var mysql = require('mysql');
-var con = mysql.createConnection({
-    user : 'naabscan',
-    password : 'a2wYdNKNLcWZ74ym',
-    database : 'naabscan',
-    socketPath : '/var/run/mysqld/mysqld.sock',
-});
-
-module.exports = con;
